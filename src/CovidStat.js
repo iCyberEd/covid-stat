2e4b43d2f2d6607ed1be283adafa0f115a09370d
Eduard Lavrynenko <lavredw@gmail.com>
2021-12-22 00:41:25 +0200

    some fixes and tweaks

--- src/CovidStat.js
+++ src/CovidStat.js
@@ -1,39 +1,65 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useCallback} from 'react'
 import Country from './Country';
 import logo from './logo.png'
+import loading from './loading.svg'
 import Search from './Search';
 
-// TODO Add loading icon before api loaded
 // TODO Add click outside modal window
 // TODO add total stats to the end or top of the list
-// TODO add link to logo to main page
 
 const rawData = []
 
 function CovidStat() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false)
   const [covidData, setCovidData] = useState([])
   const [covidElements, setCovidElements] = useState([])
   const [countryModal, setCountryModal] = useState(false)
   const [countryData, setCountryData] = useState({})
-  const [sortOrder, setSortOrder] = useState({id: "ascend"})
+  const [sortOrder, setSortOrder] = useState({Country: "ascend"})
+
+  const toggleModal = useCallback(() => {
+    setCountryModal(!countryModal)
+    document.body.style.overflow = document.body.style.overflow === "hidden" ? "visible" : "hidden"
+  }, [countryModal])
   
   useEffect(() => {
     console.log("useEffect initiation");
+    setTimeout(() => {
     fetch('https://api.covid19api.com/summary')
       .then( response => response.json() )
       .then( json => {
-        console.log(json);
+        console.log(json)
+
+        // catching error: when page not found covid19api returns json
+        if (json.message) {
+          throw json
+        }
+
         let countriesArr = json.Countries.map( (country, ind) => {
           country.id = ind + 1
           return country
         })
+
         rawData.push(...countriesArr)
         Object.freeze(rawData)
+        setIsLoaded(true)
         setCovidData([...countriesArr])
-      } )
+      })
+      .catch((error) => {
+        console.dir(error)
+        setIsLoaded(true)
+        setError(error)
+      })
+    }, 3000);
   }, [])
 
   useEffect(() => {
+    const countryClick = (country) => {
+      setCountryData(country)
+      toggleModal()
+    }
+
     console.log("useEffect covidData");
     let isAscend = Object.values(sortOrder)[0] === "ascend"
     let countriesArrEl = covidData.map( (country, ind) => {
@@ -44,7 +70,7 @@ function CovidStat() {
       </li>)
     })
     setCovidElements([...countriesArrEl])
-  }, [covidData])
+  }, [covidData, sortOrder, toggleModal])
 
 
   function covidTableSort(headerName) {
@@ -87,44 +113,48 @@ function CovidStat() {
     setCovidData([...result])
   }
 
-  function toggleModal() {
-    setCountryModal(!countryModal)
-    document.body.style.overflow = document.body.style.overflow === "hidden" ? "visible" : "hidden"
-  }
-
-  function countryClick(country) {
-    setCountryData(country)
-    toggleModal()
-  }
-
   function getHeaderClass(headerClass) {
     let thisClassName = "country-list__header"
 
     if (sortOrder.hasOwnProperty(headerClass)) {
       thisClassName += sortOrder[headerClass] === "ascend" ? " ascending" : " descending"
     }
-
     return thisClassName
   }
 
+  function renderingLoadingState() {
+    if (error) {
+      return <div className='error'>Error: {error.message}</div>
+    } else if (!isLoaded) {
+      return <div className="loading">
+        <div>Loading...</div>
+        <div><img src={loading} alt="loading" /></div>
+      </div>
+    } else {
+      return (
+      <ul className='country-list'>
+        <li className='country-list__headers'>
+          <div className='country-list__element__index'>№</div>
+          <div className={getHeaderClass("Country")} onClick={ () => covidTableSort("Country") }>Country</div>
+          <div className={getHeaderClass("TotalConfirmed")} onClick={ () => covidTableSort("TotalConfirmed") }>Total confirmed</div>
+        </li>
+        {covidElements}
+      </ul>
+      )
+    }
+  }
 
   return (
     <>
       <div className={countryModal ? "main-wrapper__modal" : "main-wrapper"}>
         <header>
-          <img src={logo} alt="COVID19 logo" className='logo'></img>
-          <h1 className='title'>STATISTIC</h1>
+          <a href="/" className='homepage-link'>
+            <img src={logo} alt="covid19statistic.com" className='logo'></img>
+            <h1 className='title'>STATISTIC</h1>
+          </a>
           <Search filterByCountry={filterByCountry.bind(this)} />
         </header>
-        
-        <ul className='country-list'>
-            <li className='country-list__headers'>
-              <div className='country-list__element__index'>№</div>
-              <div className={getHeaderClass("Country")} onClick={ () => covidTableSort("Country") }>Country</div>
-              <div className={getHeaderClass("TotalConfirmed")} onClick={ () => covidTableSort("TotalConfirmed") }>Total confirmed</div>
-            </li>
-            {covidElements}
-        </ul>
+        {renderingLoadingState()}
       </div>
 
       {countryModal && <Country countryData={countryData} toggleModal={toggleModal.bind(this)} />}
